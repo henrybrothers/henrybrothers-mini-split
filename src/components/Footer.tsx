0ced5d444a8d6e93db361ddaae02089a905983ea
henrybrothers <fixitformehank@gmail.com>
2025-05-20 17:29:58 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -48,7 +48,7 @@ const Footer = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-henry-yellow mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                 </svg>
-                <a href="mailto:info@thehenrybros.com" className="text-gray-300 hover:text-henry-yellow">hello@thehenrybros.com</a>
+                <a href="mailto:hello@thehenrybros.com" className="text-gray-300 hover:text-henry-yellow">hello@thehenrybros.com</a>
               </li>
               <li className="flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-henry-yellow mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">

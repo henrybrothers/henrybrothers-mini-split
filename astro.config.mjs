8bbf8069de4e880d36e459692fd117f9b5b2a4dd
henrybrothers <212423118+henrybrothers@users.noreply.github.com>
2026-03-25 15:48:24 -0400

    feat: conditionally include Keystatic integration based on environment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,15 @@ import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
+
+const isDev = process.env.NODE_ENV !== 'production';
+
 export default defineConfig({
   site: 'https://thehenrybros.com',
   integrations: [
     react({ include: ['**/keystatic/**'] }),
     markdoc(),
-    keystatic(),
+    ...(isDev ? [keystatic()] : []),
     sitemap({
       filter: (page) =>
         !page.includes('/schedule') && !page.includes('/keystatic'),
